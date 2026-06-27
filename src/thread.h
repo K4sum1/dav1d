58468baedcf158a862fdca5b907abc9f8898dc2c
the_r3dacted <k4sum1@eclipse.cx>
2026-06-26 21:15:16 -0700

    Reapply "e3k pthread_once"
    This reverts commit 1b92187be6922fd2b63167fcdb2821dc9b1a0606.

--- src/thread.h
+++ src/thread.h
@@ -47,7 +47,10 @@ typedef struct {
 } pthread_attr_t;
 
 typedef CRITICAL_SECTION pthread_mutex_t;
-typedef INIT_ONCE pthread_once_t;
+typedef volatile LONG pthread_once_t;
+#define MY_PTHREAD_ONCE_INIT  0
+#define MY_PTHREAD_ONCE_INPROGRESS 1
+#define MY_PTHREAD_ONCE_DONE 2
 
 enum {
 	C_SIGNAL = 0,
